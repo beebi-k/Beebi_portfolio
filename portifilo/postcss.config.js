5d1cf318403dc306fe1d8d6f9e00c93dd4d634d3
beebi950 <beebibeebi950@gmail.com>
2026-02-15 18:16:01 +0530

    Move React project to root for Vercel deployment

--- portifilo/postcss.config.js
+++ portifilo/postcss.config.js
@@ -1,6 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
   },
-}
+  plugins: [],
+};
